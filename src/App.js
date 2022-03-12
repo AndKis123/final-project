87c33b13ba07f1654395c93334d348b2390f0a35
AndKis123 <andrei.d.kiss@gmail.com>
2022-03-12 16:15:17 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './Header/Header';
-import Home from './Home/Home';
-import Products from './Products/Products';
-import ProductDetails from './ProductDetails/ProductDetails';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Cart from "./Cart/Cart";
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+import ProductDetails from "./ProductDetails/ProductDetails";
+import Products from "./Products/Products";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Header/>
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:productId" element={<ProductDetails />} />
-        <Route path="/categories" element={<div>Categories</div>} />
-        <Route path="/categories/:categoryName" element={<div>Category details</div>} />
-        <Route path="/cart" element={<div>Cart</div>} />
-        <Route path="/checkout" element={<div>Checkout</div>} />
-        <Route path="/thanks" element={<div>Thank you!</div>} />
-        <Route path="/faq" element={<div>FaQ</div>} />
-        <Route path="/contact" element={<div>Contact</div>} />
-        <Route path="*" element={<div>Not found</div>} />
-
-      </Routes>
-
-
+        <Header />
+        <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetails />} />
+          <Route path="/categories" element={<div>Categories</div>} />
+          <Route
+            path="/categories/:categoryName"
+            element={<div>Category details</div>}
+          />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<div>Checkout</div>} />
+          <Route path="/thanks" element={<div>Thank you</div>} />
+          <Route path="/faq" element={<div>FaQ</div>} />
+          <Route path="/contact" element={<div>Contact</div>} />
+          <Route path="*" element={<div>Not found</div>} />
+        </Routes>
+        </div>
       </BrowserRouter>
-
-      <footer>Footer</footer>
-
-
-
+      <div>
+      <footer /><div className="container">Footer
+      </div>
+    </div>
     </div>
   );
 }
